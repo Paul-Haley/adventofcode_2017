ffd150c5b2c97db170201c9eb52413e4b542bc35
Paul Haley <paul.haley@bigpond.com>
2017-12-05 19:28:43 +1000

    day 04

--- day04/solution2.java
+++ day04/solution2.java
@@ -1,21 +1,27 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
-public class solution1 {
+public class solution2 {
     public static int validPasswords(String file) throws IOException {
         Scanner line = new Scanner(new File(file));
         int count = 0;
         while (line.hasNextLine()) {
             Scanner words = new Scanner(line.nextLine());
-            Set<String> seen = new HashSet<String>();
+            Set<Map<Character, Integer>> seen = new HashSet<Map<Character, Integer>>();
             boolean good = true;
             while (words.hasNext()) {
-                if (!seen.add(words.next())) {
+                Map<Character, Integer> perm = new HashMap<Character, Integer>();
+                for (Character c : words.next().toCharArray()) {
+                    perm.put(c, perm.getOrDefault(c, 0) + 1);
+                }
+                
+                if (!seen.add(perm)) {
                     good = false;
-                    break;
                 }
             }
             words.close();
