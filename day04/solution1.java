6149963c9a5fa863c6b2490e1faf84223e8a4a83
Paul Haley <paul.haley@bigpond.com>
2017-12-05 19:08:24 +1000

    day04 solution1

---
+++ day04/solution1.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class solution1 {
+    public static int validPasswords(String file) throws IOException {
+        Scanner line = new Scanner(new File(file));
+        int count = 0;
+        while (line.hasNextLine()) {
+            Scanner words = new Scanner(line.nextLine());
+            Set<String> seen = new HashSet<String>();
+            boolean good = true;
+            while (words.hasNext()) {
+                if (!seen.add(words.next())) {
+                    good = false;
+                }
+            }
+            words.close();
+            count += good ? 1 : 0;
+        }       
+        line.close();
+        return count;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        System.out.println(validPasswords(args[0]));
+    }
+}
+
+
