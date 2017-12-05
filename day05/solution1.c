268575bb11eef8bffdf99b97933068eac5934faf
Paul Haley <paul.haley@bigpond.com>
2017-12-05 22:18:41 +1000

    day05 sol1

---
+++ day05/solution1.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+
+int main(int argc, char** argv) {
+    FILE* fd = fopen(argv[1], "r");
+    unsigned cap = 512;
+    unsigned cur = 0;
+    int* numbers = malloc(cap * sizeof(int));
+    char c [10];
+    while (1) {
+        if (fgets(c, 10, fd) == NULL) {
+            numbers[cur] = atoi(c);
+            break;
+        }
+        numbers[cur++] = atoi(c);
+        if (cur == cap) {
+            cap *= 2;
+            int* tmp = malloc(cap * sizeof(int));
+            memcpy(tmp, numbers, cur * sizeof(int));
+            free(numbers);
+            numbers = tmp;
+        }
+    }
+    fclose(fd);
+    int j = 0;
+    FILE* q = fopen("cats", "w");
+    for (; j < cur; ++j) {
+        fprintf(q, "%d\n", numbers[j]);
+    }
+    fprintf(q,"\n");
+    fclose(q);
+
+
+    int i = 0;
+    unsigned jumps = 0;
+    for(;;) {
+        ++jumps;
+        int old = i;
+        i += numbers[i];
+        numbers[old] += 1;
+        if (i < 0 || i >= cur) {
+            break;
+        }
+    }
+
+    printf("Jumps = %d\n", jumps);
+    return 0;
+}
+        
