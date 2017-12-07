f292c3464e7956f36006b27fcbc6aba2f1861c73
Paul Haley <paul.haley@bigpond.com>
2017-12-07 20:39:24 +1000

    day07 solution 1

---
+++ day07/day07a.py
@@ -0,0 +1,15 @@
+import sys
+file = open(sys.argv[1])
+bottom = ""
+seen = set()
+above = set()
+for line in file:
+    parts = line.split()
+    seen.add(parts[0])
+    for i in parts[3:]:
+        if (i.endswith(",")):
+            above.add(i[0:-1])
+        else:
+            above.add(i[0:])
+file.close()
+print("bottom is ", seen.difference(above))
