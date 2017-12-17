e61732859114a548799f2cb274c06fb5595c9127
Paul Haley <paul.haley@bigpond.com>
2017-12-17 15:34:07 +1000

    terminating solution to day 17 solution 2

---
+++ day17/day17b_fast.py
@@ -0,0 +1,13 @@
+sl = [-1 for i in range(50000001)]
+sl[0] = 0
+index = 0
+step = 328
+for i in range(1,50000001):
+    index = (index + step)%i
+    sl[index + 1] = i
+    index += 1
+    
+    
+for i in range(len(sl)):
+    if sl[i] == 0:
+        print(sl[i+1])
