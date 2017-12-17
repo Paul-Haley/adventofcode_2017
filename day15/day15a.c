b43683c5e83e2c445cf5b290a04e54a7001ba4da
Paul Haley <paul.haley@bigpond.com>
2017-12-17 15:39:48 +1000

    cleaned code for day15 solution 1

--- day15/day15a.c
+++ day15/day15a.c
@@ -6,7 +6,6 @@ int main(int argc, char** argv) {
     uint64_t A = 883, B = 879, Afactor = 16807, Bfactor = 48271, div = 2147483647;
     unsigned i, matches = 0;
     for(i = 0; i < 40000000; ++i) {
-    //for(i = 0; i < 5; ++i) {
         A *= Afactor;
         A %= div;
         B *= Bfactor;
