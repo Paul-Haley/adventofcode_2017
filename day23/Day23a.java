fa260cceae189f9c54c51def6702c769c2fa0dc8
Paul Haley <paul.haley@bigpond.com>
2017-12-24 15:14:24 +1000

    refactor of day23

--- day23/Day23a.java
+++ day23/Day23a.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Day22a {
+public class Day23a {
     static Map<Character, Long> initialiseRegisters() {
         Map<Character, Long> reg = new HashMap<Character, Long>();
         for (char c = 'a'; c <= 'h'; ++c) {
